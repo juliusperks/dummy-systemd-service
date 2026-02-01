b05794cdb3fd19e1e49e8673b2497890ee459b37
juliusperks <97810178+juliusperks@users.noreply.github.com>
2026-02-02 07:21:11 +1100

    update on location of scripts to local directory

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-cp ~/dummy.sh /usr/local/bin/dummy.sh
+cp dummy.sh /usr/local/bin/dummy.sh
 chmod +x /usr/local/dummy.sh
 
 touch /var/log/dummy-service.log
 chmod 644 /var/log/dummy-service.log
 
-cp ~/dummy.service /etc/systemd/system/dummy.servce
+cp dummy.service /etc/systemd/system/dummy.servce
 
 systemctl daemon-reload
