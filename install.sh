efa1d310eb0ca09ad1d07354ef9a84f48362bda6
juliusperks <97810178+juliusperks@users.noreply.github.com>
2026-02-02 07:20:04 +1100

    scripts

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cp ~/dummy.sh /usr/local/bin/dummy.sh
+chmod +x /usr/local/dummy.sh
+
+touch /var/log/dummy-service.log
+chmod 644 /var/log/dummy-service.log
+
+cp ~/dummy.service /etc/systemd/system/dummy.servce
+
+systemctl daemon-reload
